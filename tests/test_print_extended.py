2979980c6d3af4acaeaa1a6b2529229f2a86042c
Ivan Arar <ivan.arar@amphinicy.com>
2019-04-12 11:57:06 +0200

    feature/project-skeleton

---
+++ tests/test_print_extended.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestPrintExtended(unittest.TestCase):
+
+    def setUp(self):
+        """Prepare test data"""
+
+    def tearDown(cls):
+        """Destroy test data"""
+
+    def test_print_extended(self):
+        """Test functionality"""
